c46cfa2619ff5b14fc426c68b62b959633d741ae
wat_s_on <wat_s_on@outlook.jp>
2019-01-24 03:09:18 +0900

    CRUD開発

--- MvcBasic/Models/Member.cs
+++ MvcBasic/Models/Member.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
 
 namespace MvcBasic.Models
 {
     public class Member
     {
         public int Id { get; set; }
+
+        [DisplayName("氏名")]
         public string Name { get; set; }
+
+        [DisplayName("メールアドレス")]
         public string Email { get; set; }
+
+        [DisplayName("誕生日")]
         public DateTime Birth { get; set; }
+
+        [DisplayName("既婚")]
         public bool Married { get; set; }
+
+        [DisplayName("自己紹介")]
         public string Memo { get; set; }
     }
 }
